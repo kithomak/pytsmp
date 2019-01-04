1a41a311a1beca1bf82597eaf9c99cec0cd2d85b
Kit-Ho Mak <kithomak23@gmail.com>
2019-01-04 09:12:27 +0800

    Update copyright year

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,13 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+from datetime import datetime
+
 
 # -- Project information -----------------------------------------------------
 
 project = 'pytsmp'
-copyright = '2018, Kit-Ho Mak'
+copyright = '2018-' + str(datetime.today().year) + ', Kit-Ho Mak'
 author = 'Kit-Ho Mak'
 
 # The short X.Y version
