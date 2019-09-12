15d58a39e016100fb44cdcc9f6115fa7736eb2bb
Kit-Ho Mak <kithomak23@gmail.com>
2019-09-12 17:58:49 +0800

    Update documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
 
+import sphinx_rtd_theme
+
 from datetime import datetime
 
 
@@ -42,6 +44,7 @@ release = '0.3.0'
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,7 +80,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
